b92fbd8845eb402179282ffa43f40f4e6df81ae4
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-09-12 16:28:41 +0300

    submission Bite 109 @ codechalleng.es

---
+++ 109/workouts.py
@@ -0,0 +1,29 @@
+workout_schedule = {'Friday': 'Shoulders',
+                    'Monday': 'Chest+biceps',
+                    'Saturday': 'Rest',
+                    'Sunday': 'Rest',
+                    'Thursday': 'Legs',
+                    'Tuesday': 'Back+triceps',
+                    'Wednesday': 'Core'}
+rest, chill, go_train = 'Rest', 'Chill out!', 'Go train {}'
+
+
+def get_workout_motd(day: str):
+    """Title case passed in day argument (monday or MONDAY -> Monday)
+       and check if it is in the given workout_schedule dict.
+
+       If it is there retrieve the workout, if not raise a KeyError.
+
+       Return the chill or go_train variable depending the retrieved
+       workout value ('Rest' or workout bla)
+
+       Trivia: /etc/motd is a file on Unix-like systems that contains
+       a 'message of the day'"""
+    workout_day = day.lower().capitalize()
+    if workout_day in workout_schedule.keys():
+        if workout_schedule[workout_day] == 'Rest':
+            return chill
+        else:
+            return go_train.format(workout_schedule[workout_day])
+    else:
+        raise KeyError
