1822d32e8476fb38248b206328d4b187247d0e86
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-09-16 12:11:05 +0300

    submission Bite 5 @ codechalleng.es
    looked over the solutions

--- 5/names.py
+++ 5/names.py
@@ -9,23 +9,26 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 def dedup_and_title_case_names(names: list) -> list:
     """Should return a list of title cased names,
        each name appears only once"""
-    return_list = []
-    for name in names:
-        x = ""
-        for s in name.split(' '):
-            x += f" {s.capitalize()}"
-        return_list.append(x.strip())
-    return list(set(return_list))
+
+    # return_list = []
+    # for name in names:
+    #     x = ""
+    #     for s in name.split(' '):
+    #         x += f" {s.capitalize()}"
+    #     return_list.append(x.strip())
+    # return list(set(return_list))
+    return list({name.title() for name in names})
 
 
 def sort_by_surname_desc(names: list) -> list:
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    return_dic = {}
-    for s in names:
-        name, surname = s.split(" ")
-        return_dic[name] = surname
-    return [f"{x} {v}" for x, v in sorted(return_dic.items(), key=operator.itemgetter(1), reverse=True)]
+    # return_dic = {}
+    # for s in names:
+    #     name, surname = s.split(" ")
+    #     return_dic[name] = surname
+    # return [f"{x} {v}" for x, v in sorted(return_dic.items(), key=operator.itemgetter(1), reverse=True)]
+    return sorted(names, key=lambda x: x.split()[-1], reverse=True)
 
 
 def shortest_first_name(names):
@@ -33,8 +36,9 @@ def shortest_first_name(names):
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    return_list = []
-    for s in names:
-        name, surname = s.split(" ")
-        return_list.append(name)
-    return sorted(return_list, key=len)[0]
+    # return_list = []
+    # for s in names:
+    #     name, surname = s.split(" ")
+    #     return_list.append(name)
+    # return sorted(return_list, key=len)[0]
+    return min([name.split()[0] for name in names], key=len)
