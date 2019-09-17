de1c9cdf23324cc0031a7f479fb756a66f01bbb5
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-09-17 12:03:01 +0300

    submission Bite 8 @ codechalleng.es

---
+++ 8/rotate.py
@@ -0,0 +1,5 @@
+def rotate(string: str, n: int):
+    """Rotate characters in a string.
+       Expects string and n (int) for number of characters to move.
+    """
+    return string[n:] + string[:n]
