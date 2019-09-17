8db123e0a4ecc30686b17aeb5f5a5ac7070a9670
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-09-17 12:30:05 +0300

    submission Bite 15 @ codechalleng.es

---
+++ 15/enumerate_data.py
@@ -0,0 +1,14 @@
+names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
+countries = 'Australia Spain Global Argentina USA Mexico'.split()
+
+
+def enumerate_names_countries():
+    """Outputs:
+       1. Julian     Australia
+       2. Bob        Spain
+       3. PyBites    Global
+       4. Dante      Argentina
+       5. Martin     USA
+       6. Rodolfo    Mexico"""
+    for i, (val1, val2) in enumerate(zip(names, countries)):
+        print(f"{i+1}. {val1.ljust(11)}{val2}")
