018476ad5668c65a32297b38158122aabab0172f
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-11-28 12:04:50 +0200

    submission Bite 136 @ codechalleng.es

--- 133/affiliation.py
+++ 133/affiliation.py
@@ -1,8 +1,11 @@
 def generate_affiliation_link(url):
-    split_url = url.split("/")
-    for affiliate_url in split_url:
-        try:
-            if affiliate_url[0].isnumeric():
-                return f"http://www.amazon.com/dp/{affiliate_url}/?tag=pyb0f-20"
-        except IndexError:
-            pass
+    affiliate_url = url.split("dp/")[-1].split("/")[0]
+    return f"http://www.amazon.com/dp/{affiliate_url}/?tag=pyb0f-20"
+
+    # split_url = url.split("/")
+    # for affiliate_url in split_url:
+    #     try:
+    #         if affiliate_url[0].isnumeric():
+    #             return f"http://www.amazon.com/dp/{affiliate_url}/?tag=pyb0f-20"
+    #     except IndexError:
+    #         pass
