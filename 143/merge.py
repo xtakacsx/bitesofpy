30f4a58b9ffeafb3f8f5f969b73dcaf434f22f8a
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-09 14:34:46 +0200

    submission Bite 149 @ codechalleng.es

--- 143/merge.py
+++ 143/merge.py
@@ -1,3 +1,5 @@
+from collections import ChainMap
+
 NOT_FOUND = "Not found"
 
 group1 = {"tim": 30, "bob": 17, "ana": 24}
@@ -15,11 +17,14 @@ def get_person_age(name):
     else:
         return NOT_FOUND
 
-    if lower_name in group3:
-        return group3[lower_name]
-    elif lower_name in group2:
-        return group2[lower_name]
-    elif lower_name in group1:
-        return group1[lower_name]
-    else:
-        return NOT_FOUND
+    m = ChainMap(group3, group2, group1)
+    return m.get(lower_name, NOT_FOUND)
+
+    # if lower_name in group3:
+    #     return group3[lower_name]
+    # elif lower_name in group2:
+    #     return group2[lower_name]
+    # elif lower_name in group1:
+    #     return group1[lower_name]
+    # else:
+    #     return NOT_FOUND
---
+++ 149/words.py
@@ -0,0 +1,14 @@
+def sort_words_case_insensitively(words):
+    """Sort the provided word list ignoring case, and numbers last
+       (1995, 19ab = numbers / Happy, happy4you = strings, hence for
+        numbers you only need to check the first char of the word)
+    """
+    sorted_words = sorted(words, key=str.lower)
+    new_list = []
+    for i, word in enumerate(sorted_words):
+        if sorted_words[i][0].isnumeric():
+            new_list.append(word)
+    for x in new_list:
+        sorted_words.remove(x)
+
+    return sorted_words + new_list
