7668b601488e4f69accebe9376ce2422bfcf6c94
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-10 10:19:52 +0200

    submission Bite 161 @ codechalleng.es

--- 153/rounding.py
+++ 153/rounding.py
@@ -6,7 +6,9 @@ def round_up_or_down(transactions, up=True):
        If up=True (default) ronud up, else round down.
        Return a new list of rounded values
     """
-    if up:
-        return [ceil(x) for x in transactions]
-    elif up is False:
-        return [floor(x) for x in transactions]
+    # if up:
+    #     return [ceil(x) for x in transactions]
+    # elif up is False:
+    #     return [floor(x) for x in transactions]
+    func = ceil if up else floor
+    return [func(x) for x in transactions]
---
+++ 161/tree.py
@@ -0,0 +1,14 @@
+import os
+
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    f = []
+    x = []
+    for (dirpath, dirnames, filenames) in os.walk(directory):
+        f.extend(filenames)
+        x.extend(dirnames)
+
+    return len(x), len(f)
