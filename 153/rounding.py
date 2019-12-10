ac62de7b4ac70043e8a9f76e388372d82ed0b366
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-10 10:06:24 +0200

    submission Bite 153 @ codechalleng.es

---
+++ 153/rounding.py
@@ -0,0 +1,12 @@
+from math import floor, ceil
+
+
+def round_up_or_down(transactions, up=True):
+    """Round the list of transactions passed in.
+       If up=True (default) ronud up, else round down.
+       Return a new list of rounded values
+    """
+    if up:
+        return [ceil(x) for x in transactions]
+    elif up is False:
+        return [floor(x) for x in transactions]
