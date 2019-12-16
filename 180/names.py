91fd3c0eee39e7c40baf216eed093fa154675376
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-16 15:21:09 +0200

    submission Bite 180 @ codechalleng.es

---
+++ 180/names.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    df = data.splitlines()
+    del df[0]
+    for entry in df:
+        details = entry.split(",")
+        countries[details[-1]].append(f"{details[1]} {details[0]}")
+
+    return countries
