c617a5051ed03e6e5d1572747b6db508704a5701
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-17 10:51:55 +0200

    submission Bite 188 @ codechalleng.es

--- 181/order.py
+++ 181/order.py
@@ -7,7 +7,8 @@ class OrderedList:
         self._numbers = []
 
     def add(self, num):
-        self._numbers.insert(bisect.bisect(self._numbers, num), num)
+        # self._numbers.insert(bisect.bisect(self._numbers, num), num)
+        bisect.insort(self._numbers, num)
 
     def __str__(self):
         return ', '.join(str(num) for num in self._numbers)
