93d00bfe490fe08c310163f23a58f527b313710e
Paul-Andrei Takács <paul-andrei.takacs@ubisoft.com>
2019-12-18 15:11:04 +0200

    UPDATE Bite 189 "The continue statement continues with the next iteration of the loop."

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -6,11 +6,19 @@ MAX_NAMES = 5
 
 
 def filter_names(names: List[str]):
-    test_list = []
+    # test_list = []
+    # for name in names:
+    #     if name[0].lower() == QUIT_CHAR:
+    #         break
+    #     if name[0].lower() != IGNORE_CHAR and not any(char.isdigit() for char in name):
+    #         test_list.append(name)
+    #
+    # return test_list[:MAX_NAMES]
+    count = 0
     for name in names:
-        if name[0].lower() == QUIT_CHAR:
+        if name.startswith(IGNORE_CHAR) or any(char.isdigit() for char in name):
+            continue
+        elif name.startswith(QUIT_CHAR) or count >= MAX_NAMES:
             break
-        if name[0].lower() != IGNORE_CHAR and not any(char.isdigit() for char in name):
-            test_list.append(name)
-
-    return test_list[:MAX_NAMES]
+        count += 1
+        yield name
