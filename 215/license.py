424314cf2f92434df53df7a0f7cad6b7f45d8c2a
Paul-Andrei Takács <andreitakacs@gmail.com>
2020-01-01 14:12:02 +0200

    submission Bite 218 @ codechalleng.es
    Signed-off-by: Paul-Andrei Takács <andreitakacs@gmail.com>

--- 215/license.py
+++ 215/license.py
@@ -5,8 +5,5 @@ def validate_license(key: str) -> bool:
     """Write a regex that matches a PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
     """
-    valid = re.compile(r"[PB]{2}-[A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8}-[A-Z0-9]{8}$")
-    if valid.match(key):
-        return True
-    else:
-        return False
+    valid = re.compile(r"^PB-[A-Z0-9]{8}(?:-[A-Z0-9]{8}){3}$")
+    return bool(valid.match(key))
