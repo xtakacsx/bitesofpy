6970b04fb32f70e4545d4464820bd7ce25b0fde1
Paul-Andrei Takács <andreitakacs@gmail.com>
2020-03-20 18:20:11 +0200

    submission Bite 51 @ codechalleng.es
    Signed-off-by: Paul-Andrei Takács <andreitakacs@gmail.com>

---
+++ 51/miller.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+# https://pythonclock.org/
+PY2_DEATH_DT = datetime(year=2020, month=1, day=1)
+BITE_CREATED_DT = datetime.strptime('2018-02-26 23:24:04', '%Y-%m-%d %H:%M:%S')
+
+
+def py2_earth_hours_left(start_date=BITE_CREATED_DT):
+    """Return how many hours, rounded to 2 decimals, Python 2 has
+       left on Planet Earth (calculated from start_date)"""
+    return round((PY2_DEATH_DT - start_date) / timedelta(hours=1), 2)
+
+
+def py2_miller_min_left(start_date=BITE_CREATED_DT):
+    """Return how many minutes, rounded to 2 decimals, Python 2 has
+       left on Planet Miller (calculated from start_date)"""
+    return round((PY2_DEATH_DT - start_date) / timedelta(minutes=61320), 2)
