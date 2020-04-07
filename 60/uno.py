524495b373a9ff43f5864e6c99704060ed9fc401
Paul-Andrei Takács <andreitakacs@gmail.com>
2020-04-07 10:27:33 +0300

    submission Bite 60 @ codechalleng.es
    Signed-off-by: Paul-Andrei Takács <andreitakacs@gmail.com>

---
+++ 60/uno.py
@@ -0,0 +1,22 @@
+from collections import namedtuple
+
+SUITS = 'Red Green Yellow Blue'.split()
+
+UnoCard = namedtuple('UnoCard', 'suit name')
+
+
+def create_uno_deck():
+    """Create a deck of 108 Uno cards.
+       Return a list of UnoCard namedtuples
+       (for cards w/o suit use None in the namedtuple)"""
+    main_deck = []
+    cards = [x for x in range(10)] + ["Draw Two", "Skip", "Reverse"]
+    for suit in SUITS:
+        for i in cards:
+            count = 1 if i == 0 else 2
+            for j in range(count):
+                main_deck.append(UnoCard(suit=suit, name=str(i)))
+    for _ in range(4):
+        main_deck.append(UnoCard(suit=None, name="Wild"))
+        main_deck.append(UnoCard(suit=None, name="Draw Four"))
+    return main_deck
